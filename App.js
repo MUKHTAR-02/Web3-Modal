dea27256086f5ea2d219bb51214dc3497dc953e9
MUKHTAR KHAN <106866680+MUKHTAR-02@users.noreply.github.com>
2023-04-23 23:00:49 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,55 +1,54 @@
 import {
-  EthereumClient, 
-  w3mConnectors, 
-  w3mProvider
-  } from "@web3modal/ethereum";
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
 
-  // import WalletCard from './WalletCard';
+// import WalletCard from './WalletCard';
 
-  import { Web3Modal, Web3Button } from "@web3modal/react";
+import { Web3Modal, Web3Button } from "@web3modal/react";
+
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+
+import { arbitrum, mainnet, polygon } from "wagmi/chains";
+import "./App.css";
+
+// import { useAccount, useContract, useSigner } from 'wagmi'
+
+function App() {
+  const chains = [arbitrum, mainnet, polygon];
+  const projectId = process.env.REACT_APP_PROJECT_ID;
+
+  // wagmi client
+  const { provider } = configureChains(chains, [w3mProvider({ projectId })]);
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors: w3mConnectors({
+      projectId,
+      version: 2,
+      appName: "web3Modal",
+      chains,
+    }),
+    provider,
+  });
   
-  import { configureChains, createClient, WagmiConfig } from "wagmi";
-  
-  import { arbitrum, mainnet, polygon } from "wagmi/chains";
-  import './App.css';
-  
-  // import { useAccount, useContract, useSigner } from 'wagmi'
 
-  function App() {
-    const chains = [arbitrum, mainnet, polygon];
-    const projectId = process.env.REACT_APP_PROJECT_ID;
-
-    // wagmi client
-    const {provider} = configureChains(chains, [
-      w3mProvider({ projectId }),
-    ]);
-    const wagmiClient = createClient({
-      autoConnect: true,
-      connectors: w3mConnectors({
-        projectId,
-        version: 2,
-        appName: "web3Modal",
-        chains,
-      }),
-      provider,
-    });
-
-    // web3modal Ethereum client
-    const ethereumClient = new EthereumClient(wagmiClient, chains);
+  // web3modal Ethereum client
+  const ethereumClient = new EthereumClient(wagmiClient, chains);
 
   return (
-  <div className="App">
-  <WagmiConfig client = {wagmiClient}>
-    <h1>Web3Modal Demo</h1>
-    <Web3Button />
-  </WagmiConfig>
-
-  <Web3Modal
-    projectId = {projectId}
-    ethereumClient = {ethereumClient}
-    />
-  </div>
+    <div className="App">
+      <WagmiConfig client={wagmiClient}>
+        <div class="imageOfMetamask">
+          <button>
+            <img src="metamask logo.jpg" alt="" srcset="" />
+          </button>
+        </div>
+      </WagmiConfig>
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+      <Web3Button />
+    </div>
   );
 }
-  
-  export default App;
+
+export default App;
